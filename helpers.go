475725504b2f020a91ae846848996d3d787382ad
Jon Wear <jon@dramafever.com>
2016-01-04 22:08:45 -0500

    initial setup.  still need to get the init code right

---
+++ helpers.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/genghisjahn/dockertools/docker"
+	_ "github.com/lib/pq"
+)
+
+type DBInfo struct {
+	ContainerName string
+	Host          string
+	DBName        string
+	UserName      string
+	Password      string
+}
+
+func getDBConn(info DBInfo) (*sql.DB, error) {
+	return sql.Open("postgres", fmt.Sprintf("host=%s user=%s dbname=%s password=%s sslmode=disable", info.Host, info.UserName, info.DBName, info.Password))
+}
+
+func setup(name string) (bool, error) {
+	var err error
+
+	info, infoErr := docker.InspectContainer(name)
+	if infoErr != nil {
+		if _, ok := infoErr.(*docker.ContainerNotFoundError); !ok {
+			return false, infoErr
+		}
+	}
+	if info.State.Running {
+		log.Printf("Container %s is already running. Started At: %s\n", name, info.State.StartedAt)
+		return true, nil
+	}
+
+	err = setupDBContainer()
+	if err != nil {
+		return false, err
+	}
+	err = initData()
+	if err != nil {
+		return false, err
+	}
+	return false, nil
+}
+
+var dbInfo DBInfo
+
+func setupDBContainer() error {
+	hostip, err := docker.GetHostIP("dev")
+	if err != nil {
+		return err
+	}
+
+	dbInfo.ContainerName = "dockerDemoDB"
+	dbInfo.Host = hostip
+	dbInfo.DBName = "dockerdemo"
+	dbInfo.UserName = "demo"
+	dbInfo.Password = "abcd1234"
+
+	argtemplate := "-p 5432:5432  --name %s  -e POSTGRES_PASSWORD=%s -e POSTGE_DB=%s -e POSTGRES_USER=%s -d postgres"
+	runargs := fmt.Sprintf(argtemplate, dbInfo.ContainerName, dbInfo.Password, dbInfo.DBName, dbInfo.UserName)
+	//runargs := "-p 5432:5432  --name dockerDemoDB  -e POSTGRES_PASSWORD=abcd1234 -e POSTGE_DB=dockerdemo -e POSTGRES_USER=demo -d postgres"
+	runErr := docker.Run(runargs, false)
+	if runErr != nil {
+		return runErr
+	}
+	return nil
+}
+
+func shutdown() error {
+	var err error
+	err = docker.StopContainer(dbInfo.ContainerName, false)
+	if err != nil {
+		return err
+	}
+	err = docker.RemoveContainer(dbInfo.ContainerName, false)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func initData() error {
+	hostip, err := docker.GetHostIP("dev")
+	if err != nil {
+		return err
+	}
+	constr := fmt.Sprintf("host=%v user=%v password=%v dbname=%v sslmode=disable", hostip, "FIXTHIS", "abcd1234", "FIXTHIS")
+	maxAttempts := 20
+	cn, _ := sql.Open("postgres", constr)
+	defer cn.Close()
+	var errExec error
+	for attempts := 1; attempts <= maxAttempts; attempts++ {
+		errPing := cn.Ping()
+		if errPing == nil {
+			log.Println("DB connect succeeded!")
+			break
+		}
+		log.Println(errPing)
+		time.Sleep(time.Duration(attempts) * time.Second)
+	}
+	_, errExec = cn.Exec("INIT DATA")
+	if errExec != nil {
+		return errExec
+	}
+	_, errExec = cn.Exec("Insert DATA")
+	return errExec
+}
---
+++ integration_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"testing"
+)
+
+func TestInsertRecrod(t *testing.T) {
+
+}
+
+func TestMain(m *testing.M) {
+	persistDB := flag.Bool("persistdb", false, "True, leave the DB container running")
+	flag.Parse()
+	keepDB, errSetup := setup("DBNAME")
+	if errSetup != nil {
+		log.Println("Setup Error:", errSetup)
+	}
+	var code int
+	if errSetup == nil {
+		code = m.Run()
+	}
+	args := os.Args
+	for _, v := range args {
+		if v == "-persist=true" {
+			*persistDB = true
+			break
+		}
+	}
+	if !*persistDB && !keepDB {
+		errShutdown := shutdown()
+		if errShutdown != nil {
+			log.Println(errShutdown)
+		}
+	}
+	os.Exit(code)
+}
